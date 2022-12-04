cec335c06a3f520572fd867a59810ba395b59f6a
neerajSingh0510 <niraj.singh@moveinsync.com>
2022-12-05 05:24:28 +0530

    prod file changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,10 @@ export const environment = {
     messagingSenderId: '899497983198',
     measurementId: 'G-YB7MREHWJV',
   },
+  PHONE_API: 'https://phonevalidation.abstractapi.com/',
+  PHONE_API_KEY: '3f0680d1d52b411e805642106b3a13d6',
+  PINCODE_API: 'https://api.postalpincode.in/pincode/',
+  WEATHER_API: 'https://api.openweathermap.org/data/2.5/weather?',
+  WEATHER_API_KEY: 'ff1bc4683fc7325e9c57e586c20cc03e',
   production: true
 };
